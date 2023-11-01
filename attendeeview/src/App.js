c35d9f27bff52a87710aaf8270e830a5e334c727
unknown <jianinc8@uci.edu>
2023-11-01 14:50:18 -0700

    implement materialUI

--- attendeeview/src/App.js
+++ attendeeview/src/App.js
@@ -1,6 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
 const App = () => {
   const [sponsors, setSponsors] = useState([]);
   const [gifts, setGifts] = useState([]);
@@ -59,15 +78,50 @@ const Sponsors = ({ sponsors }) => (
 const Gifts = ({ gifts }) => (
   <div className="gifts">
     <h2>Your gifts from our amazing sponsors!</h2>
-    {gifts.map((gift) => (
-      <div className="gift" key={gift.id}>
-        <img src={gift.logo} alt = {" "}  />
-        <span>{gift.gift_name}</span>
-        <p>{gift.description}</p>
-      </div>
-    ))}
+    <div className='gifts-img'>
+      <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {gifts.map((gift) => (
+              <Grid item key={gift.id} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+        
+          ))}
+        </Grid>
+      </Container>
+    </div>
   </div>
 );
+    // <div className="gift" key={gift.id}>
+        //   <img src={gift.logo} alt = {" "}  />
+        //   <span>{gift.gift_name}</span>
+        //   <p>{gift.description}</p>
+        // </div>
 
 const ShippingInfo = () => (
   <div className="shipping-info">
