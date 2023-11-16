be6b8db9c165d5110976d056b28f45e6effd7446
jianinc8 <91291939+jianinc8@users.noreply.github.com>
2023-11-16 11:49:11 -0800

    Implement autocomplete for states

--- attendeeview/src/ShippingInfoModal.js
+++ attendeeview/src/ShippingInfoModal.js
@@ -5,10 +5,18 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import Grid from '@mui/material/Grid';
 import { useAuth } from './authContext';
 
-
+const allStates = [
+  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+  'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+  'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+  'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+  'West Virginia', 'Wisconsin', 'Wyoming',
+];
 
 function ShippingInfoModal({selectedGift}) {
   const { user, signIn, signOut } = useAuth();
@@ -56,105 +64,111 @@ function ShippingInfoModal({selectedGift}) {
     return (
       <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
-          <TextField
-              required
-              id="firstName"
-              name="first_name"
-              label="First name"
-              fullWidth
-              autoComplete="given-name"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.first_name}
-          />
+            <TextField
+                required
+                id="firstName"
+                name="first_name"
+                label="First name"
+                fullWidth
+                autoComplete="given-name"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.first_name}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField
-              required
-              id="lastName"
-              name="last_name"
-              label="Last name"
-              fullWidth
-              autoComplete="family-name"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.last_name}
-          />
+            <TextField
+                required
+                id="lastName"
+                name="last_name"
+                label="Last name"
+                fullWidth
+                autoComplete="family-name"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.last_name}
+            />
           </Grid>
           <Grid item xs={12}>
-          <TextField
-              required
-              id="address1"
-              name="address1"
-              label="Address line 1"
-              fullWidth
-              autoComplete="shipping address-line1"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.address1}
-          />
+            <TextField
+                required
+                id="address1"
+                name="address1"
+                label="Address line 1"
+                fullWidth
+                autoComplete="shipping address-line1"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.address1}
+            />
           </Grid>
           <Grid item xs={12}>
-          <TextField
-              id="address2"
-              name="address2"
-              label="Address line 2"
-              fullWidth
-              autoComplete="shipping address-line2"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.address2}
-          />
+            <TextField
+                id="address2"
+                name="address2"
+                label="Address line 2"
+                fullWidth
+                autoComplete="shipping address-line2"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.address2}
+            />
+            </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                required
+                id="city"
+                name="city"
+                label="City"
+                fullWidth
+                autoComplete="shipping address-level2"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.city}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField
-              required
-              id="city"
-              name="city"
-              label="City"
-              fullWidth
-              autoComplete="shipping address-level2"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.city}
-          />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-          <TextField
-              id="state"
-              name="state"
-              label="State/Province/Region"
-              fullWidth
-              variant="standard"
+            <Autocomplete
+              options={allStates}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  id="state"
+                  name="state"
+                  label="State/Province/Region"
+                  fullWidth
+                  variant="standard"
+                />
+              )}
               onChange={handleInputChange}
               value={shippingInfo.state}
-          />
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField
-              required
-              id="zip"
-              name="zip"
-              label="Zip / Postal code"
-              fullWidth
-              autoComplete="shipping postal-code"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.zip}
-          />
+            <TextField
+                required
+                id="zip"
+                name="zip"
+                label="Zip / Postal code"
+                fullWidth
+                autoComplete="shipping postal-code"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.zip}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField
-              required
-              id="country"
-              name="country"
-              label="Country"
-              fullWidth
-              autoComplete="shipping country"
-              variant="standard"
-              onChange={handleInputChange}
-              value={shippingInfo.country}
-          />
+            <TextField
+                required
+                id="country"
+                name="country"
+                label="Country"
+                fullWidth
+                autoComplete="shipping country"
+                variant="standard"
+                onChange={handleInputChange}
+                value={shippingInfo.country}
+            />
           </Grid>
           <Button onClick={handleSubmit} color="primary">
               Save
